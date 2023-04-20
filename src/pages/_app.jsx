cf742749f6ca740e55356aa446a6faa16b1af0e9
DenisPetyhov <91735662+DenisPetyhov@users.noreply.github.com>
2023-04-20 22:26:43 +0300

    change favicon

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -31,13 +31,13 @@ export default function App({ Component, pageProps }) {
         <title>My daily app</title>
         <link rel="manifest" href="/manifest.json" />
         <link
-          href="/icons/favicon-16x16.png"
+          href="/icons/icon-16x16.png"
           rel="icon"
           type="image/png"
           sizes="16x16"
         />
         <link
-          href="/icons/favicon-32x32.png"
+          href="/icons/icon-32x32.png"
           rel="icon"
           type="image/png"
           sizes="32x32"
